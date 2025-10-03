eaa194dbc561b04747794a60ddb606f518ab3267
Arthur Campos <138174613+arthurd3@users.noreply.github.com>
2025-10-02 22:47:54 -0300

    Delete Main directory

--- Main/src/main/java/com/q1/Main.java
+++
@@ -1,8 +0,0 @@
-package com.q1;
-
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- Main/src/main/java/com/q2/Turma.java
+++
@@ -1,5 +0,0 @@
-package com.q2;
-
-public class Turma {
-    Aluno[] alunos = new Aluno[5];
-}
